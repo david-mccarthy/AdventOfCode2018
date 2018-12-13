5f4717452734ec4d28faafb8f103d3d46cbc5a28
David McCarthy <David.McCarthy@openjawtech.com>
2018-12-13 08:48:33 +0000

    Refactor with javadocs and moving common setup code to constructor.

--- src/main/java/com/davemac/aoc/day5/AlchemicalReduction.java
+++ src/main/java/com/davemac/aoc/day5/AlchemicalReduction.java
@@ -9,47 +9,68 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class AlchemicalReduction {
+    private String[] split;
 
-    public int solvePartOne(File file) {
+    /**
+     * Constructor.
+     * Read in the input chars and store in an array.
+     *
+     * @param file Your input file.
+     */
+    public AlchemicalReduction(File file) {
         Scanner scanner = FileUtils.getScanner(file);
         String input = scanner.nextLine();
         scanner.close();
-        String[] split = input.split("");
+        split = input.split("");
 
+    }
+
+    /**
+     * Part 1.
+     *
+     * @return The size of the processed input string.
+     */
+    public int solvePartOne() {
         return process(split).size();
     }
 
-    public int solvePartTwo(File file) {
-        Scanner scanner = FileUtils.getScanner(file);
-        String input = scanner.nextLine();
-
-        scanner.close();
-        String[] split = input.split("");
-
-        return processPartTwo(split);
-    }
-
-    private int processPartTwo(String[] l) {
+    /**
+     * Part 2.
+     *
+     * @return The size of the shortest processed input string.
+     */
+    public int solvePartTwo() {
         String[] chars = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
         int shortestReduction = Integer.MAX_VALUE;
         for (String c : chars) {
-            Stack<String> s = process(l, c);
+            Stack<String> s = process(split, c);
             List<String> strings = new ArrayList<>(s);
             Stack<String> reducedStack = process(strings.toArray(new String[0]));
             int size = reducedStack.size();
+
             if (size < shortestReduction) {
                 shortestReduction = size;
             }
         }
+        
         return shortestReduction;
     }
 
+    /**
+     * Create a stack of the given input, where all characters matching {@code match}, ignoring case, are removed.
+     *
+     * @param list  Input list.
+     * @param match The character to match.
+     * @return Processed stack.
+     */
     private Stack<String> process(String[] list, String match) {
         Stack<String> stack = new Stack<>();
         for (String s : list) {
             stack.push(s);
+
             if (stack.size() >= 1) {
                 String a = stack.pop();
+
                 if (!(match.equalsIgnoreCase(a))) {
                     stack.push(a);
                 }
@@ -59,6 +80,13 @@ public class AlchemicalReduction {
         return stack;
     }
 
+    /**
+     * Create a stack from the given input, where there are no adjacent characters that match.
+     * A match is in the form aA, where the character is the same, but the case is different.
+     *
+     * @param list The input list.
+     * @return Processed stack.
+     */
     private Stack<String> process(String[] list) {
         Stack<String> stack = new Stack<>();
         for (String s : list) {
@@ -75,6 +103,14 @@ public class AlchemicalReduction {
         return stack;
     }
 
+    /**
+     * Do the 2 provided inputs match.
+     * Inputs match if they are the same character, in different case, i.e. Aa, bB but not AA or bb.
+     *
+     * @param s1 Input 1.
+     * @param s2 Input 2.
+     * @return True if matching inputs.
+     */
     private boolean matches(String s1, String s2) {
         if (isUpperCase(s1)) {
             return (!isUpperCase(s2)) && s1.equalsIgnoreCase(s2);
@@ -83,8 +119,13 @@ public class AlchemicalReduction {
         }
     }
 
+    /**
+     * Is the given input in upper case?
+     *
+     * @param s Input.
+     * @return True if the input is in upper case.
+     */
     private boolean isUpperCase(String s) {
         return s.toUpperCase().equals(s);
     }
-
 }
--- src/test/java/com/davemac/aoc/day5/AlchemicalReductionTest.java
+++ src/test/java/com/davemac/aoc/day5/AlchemicalReductionTest.java
@@ -13,25 +13,24 @@ public class AlchemicalReductionTest {
     private static AlchemicalReduction alchemicalReduction;
     @BeforeClass
     public static void setupClass(){
-           alchemicalReduction = new AlchemicalReduction();
+           alchemicalReduction = new AlchemicalReduction(INPUT_FILE);
     }
 
     @Test
     public void testPartOne(){
-        int s = alchemicalReduction.solvePartOne(INPUT_FILE);
+        int s = alchemicalReduction.solvePartOne();
         TestUtils.report("5","1",Integer.toString(s));
     }
 
     @Test
     public void testPartTwo(){
-        int s = alchemicalReduction.solvePartTwo(INPUT_FILE);
+        int s = alchemicalReduction.solvePartTwo();
         TestUtils.report("5","2", Integer.toString(s));
     }
 
     @Test
     public void testPartOneWithSampleData() {
-        int s = alchemicalReduction.solvePartOne(SAMPLE_FILE);
+        int s = new AlchemicalReduction(SAMPLE_FILE).solvePartOne();
         System.out.println(s);
     }
-
 }
